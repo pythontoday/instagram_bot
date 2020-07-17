c7986085a7d18bbd924f25303eb982a6b324da0c
pythontoday <pythontodaynet@gmail.com>
2020-07-17 19:08:03 +0700

    Initial commit

---
+++ lesson_1/lesson_1.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from auth_data import username, password
+import time
+import random
+
+
+def login(username, password):
+    browser = webdriver.Chrome('../chromedriver/chromedriver')
+
+    try:
+        browser.get('https://www.instagram.com')
+        time.sleep(random.randrange(3, 5))
+
+        username_input = browser.find_element_by_name('username')
+        username_input.clear()
+        username_input.send_keys(username)
+
+        time.sleep(2)
+
+        password_input = browser.find_element_by_name('password')
+        password_input.clear()
+        password_input.send_keys(password)
+
+        password_input.send_keys(Keys.ENTER)
+        time.sleep(10)
+
+        browser.close()
+        browser.quit()
+    except Exception as ex:
+        print(ex)
+        browser.close()
+        browser.quit()
+
+
+login(username, password)
